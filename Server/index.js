f61bd55826243d3f95a2dadd4e2098fa0c5e2de2
yash03rathore <rathoreyash2003@gmail.com>
2024-03-08 15:59:24 -0800

    resolved cors error

--- Server/index.js
+++ Server/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors(
     {
         origin: ["https://todo-list-frontend-mocha.vercel.app"],
-        methods: ["POST", "GET"],
+        methods: ["POST", "GET", "PUT", "DELETE"],
         credentials: true
     }
 ))
