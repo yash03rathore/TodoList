5eb4b962ebe2a606b3d8802a59f10bada7a37650
yash03rathore <rathoreyash2003@gmail.com>
2024-03-08 15:43:50 -0800

    resolved cors error

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,7 @@ const TodoModel = require('./Models/Todo')
 const app = express()
 app.use(cors(
     {
-        origin: ["https://todo-list-frontend-mocha.vercel.app/"],
+        origin: ["https://todo-list-frontend-mocha.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
